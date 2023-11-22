35cdeec1d5bf902843e5bf98bd20151298e0582e
iraj.habibzadeh <iraj.habibzadeh70@gmail.com>
2023-11-23 02:25:19 +0330

    create v1dashboard and make axios requests for the cryptocurrency

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,31 @@
 <template>
- <div :id="$route.name">
-  {{ $route.name }}
-</div>
+  <div :id="$route.name">
+    {{ $route.name }}
+  </div>
 </template>
 
 <script >
 
 export default {
   name: 'IndexPage',
+
+  created() {
+    this.getCurrncy()
+  },
+  methods: {
+    getCurrncy() {
+      this.$axios.$get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+        headers: {
+         
+          'X-CMC_PRO_API_KEY': '97b04997-62c8-4f3a-a922-c08b03a06832',
+        },
+      }).then((res) => {
+        console.log(res.data, "res")
+      }).catch((err) => console.log(err, "err"))
+
+    }
+
+  }
 }
 </script>
 <style lang="scss"></style> 
